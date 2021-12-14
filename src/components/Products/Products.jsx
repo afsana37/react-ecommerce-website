8aa8b980b5d2af8641f8a78eea75be6c7ab33abd
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-14 18:12:46 +0100

    Navbar created

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {id:1, name:'Shoes', description: 'Running shoes.', price: '$5', image:"https://cdn.pixabay.com/photo/2015/05/31/10/54/shoes-791044_960_720.jpg"},
@@ -9,8 +10,10 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
     return(
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify= "center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
