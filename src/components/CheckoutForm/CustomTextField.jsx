36ecaec215f8b9fa39fee1fa620a32b9213b7bde
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-15 22:23:40 +0100

    Address From created

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,59 @@
+// import React from 'react'
+// import { TextField, Grid } from '@material-ui/core';
+// import { useFormContext, Controller } from 'react-hook-form';
+
+// const FormInput = ({name, label, required}) => {
+  
+//     return (
+
+//         <Grid item xs={12} sm={6}>
+//             <Controller
+//                 as={TextField}
+//                 name={name}
+//                 control={control}
+//                 label={label}
+//                 fullWidth
+//                 required={required}
+//             />
+//         </Grid>   
+
+//         // <Grid item xs={12} sm={6}>
+//         //     {/* <Controller
+//         //         as={TextField}
+//         //         control={control}
+//         //         fullWidth
+//         //         name={name}
+//         //         label={label}
+//         //         required={required}
+//         //     /> */}
+            
+//         // </Grid>
+//     )
+// }
+
+// export default FormInput
+
+
+import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { TextField, Grid } from '@material-ui/core';
+
+function FormInput({ name, label, required }) {
+  const { control } = useFormContext();
+ 
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        as={TextField}
+        name={name}
+        control={control}
+        label={label}
+        fullWidth
+        required={required}
+      />
+    </Grid>
+  );
+}
+
+export default FormInput;
