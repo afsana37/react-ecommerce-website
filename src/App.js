fa77a36a66bc315f769753032e58d86fcfe99c99
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-15 21:22:16 +0100

    Checkout implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {commerce} from './lib/commerce';
-import {Products, Navbar, Cart} from './components';
+import {Products, Navbar, Cart, Checkout} from './components';
 import {BrowserRouter as  Router, Switch, Route} from 'react-router-dom'
 
 
@@ -66,11 +66,15 @@ const App = () => {
                             handleEmptyCart={handleEmptyCart} 
                         />
                     </Route>
+
+                    <Route exact path="/checkout">
+                        <Checkout />
+                    </Route>
                 </Switch>
                 
             </div>
         </Router>
-    )
+    );
 }
 
 export default App
