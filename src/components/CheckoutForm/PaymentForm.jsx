eb3d65c314ebbc384764ee94d38fa9e55584b473
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-18 14:23:12 +0100

    Payment form completed

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,21 +1,53 @@
 import React from 'react'
 import { Typography, Button, Divider } from '@material-ui/core';
-import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { Elements,ElementsConsumer, CardElement } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
 import Review from './Review';
 
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-const PaymentForm = ({checkoutToken}) => {
+const PaymentForm = ({checkoutToken, backStep, onCaptureCheckout, nextStep, shippingData}) => {
+    const handleSubmit = async (event, elements, stripe) => {
+        event.preventDefault();
+
+        if(!stripe || !elements) return;
+
+        const CardElement = elements.getElement(CardElement);
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({type: 'card', card: CardElement})
+        if (error) {
+            console.log('[error]', error);
+          } else {
+            const orderData = {
+              line_items: checkoutToken.live.line_items,
+              customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
+              shipping: { name: 'International', street: shippingData.address1, town_city: shippingData.city, county_state: shippingData.shippingSubdivision, postal_zip_code: shippingData.zip, country: shippingData.shippingCountry },
+              fulfillment: { shipping_method: shippingData.shippingOption },
+              payment: {
+                gateway: 'stripe',
+                stripe: {
+                  payment_method_id: paymentMethod.id,
+                },
+              },
+            };
+
+            onCaptureCheckout(checkoutToken.id, orderData);
+
+            nextStep();
+        }
+
+
+    }
     return (
         <>
             <Review checkoutToken={checkoutToken} />
-            {/* <Divider />
+            <Divider />
             <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment method</Typography>
             <Elements stripe={stripePromise}>
                 <ElementsConsumer>
                     {({ elements, stripe }) => (
-                        <form >
+                        <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
                             <CardElement />
                             <br /> <br />
                     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -27,9 +59,10 @@ const PaymentForm = ({checkoutToken}) => {
                 </form>
                 )}
                 </ElementsConsumer>
-            </Elements> */}
+            </Elements>
         </>
     )
 }
+
 
 export default PaymentForm
